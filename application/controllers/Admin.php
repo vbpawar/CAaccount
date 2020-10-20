2d404a8b7dd9071cdda8380ea613a2a90a267bb2
Lalit <Lalit@Lalit-PC>
2020-10-20 12:21:01 +0530

    CA Certificate

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,12 +2,16 @@
 
 class Admin extends CI_Controller {
 
-        public function formDemo()
-        {
-                $this->load->view('admin/form_demo');
-                
-        }
-        public function caCertificate() {
-            $this->load->view('admin/ca_certificate');
-        }
+    public function formDemo() {
+        $this->load->view('admin/header');
+        $this->load->view('admin/form_demo');
+        $this->load->view('admin/footer');
+    }
+
+    public function caCertificate() {
+        $this->load->view('admin/header');
+        $this->load->view('admin/ca_certificate');
+        $this->load->view('admin/footer');
+    }
+
 }
--- application/views/admin/form_demo.php
+++ application/views/admin/form_demo.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Forms Layout | SmartAdmin - Responsive admin template.</title>
-        <link rel="shortcut icon" href="<?php echo base_url('admin_assets/img/favicon.ico');?>">
-        <!--STYLESHEET-->
-        <!--=================================================-->
-        <!--Roboto Slab Font [ OPTIONAL ] -->
-        <link href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,100,700" rel="stylesheet">
-        <link href="http://fonts.googleapis.com/css?family=Roboto:500,400italic,100,700italic,300,700,500italic,400" rel="stylesheet">
-        <!--Bootstrap Stylesheet [ REQUIRED ]-->
-        <link href="<?php echo base_url('admin_assets/css/bootstrap.min.css');?>" rel="stylesheet">
-        <!--Jasmine Stylesheet [ REQUIRED ]-->
-        <link href="<?php echo base_url('admin_assets/css/style.css');?>" rel="stylesheet">
-        <!--Font Awesome [ OPTIONAL ]-->
-        <link href="<?php echo base_url('admin_assets/plugins/font-awesome/css/font-awesome.min.css');?>" rel="stylesheet">
-        <!--Switchery [ OPTIONAL ]-->
-        <link href="<?php echo base_url('admin_assets/plugins/switchery/switchery.min.css');?>" rel="stylesheet">
-        <!--Bootstrap Select [ OPTIONAL ]-->
-        <link href="<?php echo base_url('admin_assets/plugins/bootstrap-select/bootstrap-select.min.css');?>" rel="stylesheet">
-        <!--Demo [ DEMONSTRATION ]-->
-        <link href="<?php echo base_url('admin_assets/css/demo/jasmine.css');?>" rel="stylesheet">
-        <!--/STYLESHEET-->
-        <!--=================================================-->
-        <!--Page Load Progress Bar [ OPTIONAL ]-->
-        <link href="<?php echo base_url('admin_assets/plugins/pace/pace.min.css');?>" rel="stylesheet">
-        <script src="<?php echo base_url('admin_assets/plugins/pace/pace.min.js');?>"></script>
-    </head>
-    <body>
+
         <div id="container" class="effect mainnav-full">
             <!--NAVBAR-->
             <!--===================================================-->
@@ -269,7 +239,7 @@
                     <!--================================-->
                     <!--End Navbar Dropdown-->
                     <!--include code-->
-                    <?php $this->view('admin/menubar'); ?>
+                    <?php $this->view('admin/navigation/menubar'); ?>
                     <!--include code-->
                     <!-- end navbar navbar-default megamenu -->
                 </div>
@@ -437,68 +407,4 @@
                 <!--===================================================-->
                 <!--END CONTENT CONTAINER-->
             </div>
-            <!-- FOOTER -->
-            <!--===================================================-->
-            <footer id="footer">
-                <!-- Visible when footer positions are fixed -->
-                <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-                <div class="show-fixed pull-right">
-                    <ul class="footer-list list-inline">
-                        <li>
-                            <p class="text-sm">SEO Proggres</p>
-                            <div class="progress progress-sm progress-light-base">
-                                <div style="width: 80%" class="progress-bar progress-bar-danger"></div>
-                            </div>
-                        </li>
-                        <li>
-                            <p class="text-sm">Online Tutorial</p>
-                            <div class="progress progress-sm progress-light-base">
-                                <div style="width: 80%" class="progress-bar progress-bar-primary"></div>
-                            </div>
-                        </li>
-                        <li>
-                            <button class="btn btn-sm btn-dark btn-active-success">Checkout</button>
-                        </li>
-                    </ul>
-                </div>
-                <!-- Visible when footer positions are static -->
-                <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-                <div class="hide-fixed pull-right pad-rgt">Currently v2.2</div>
-                <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-                <!-- Remove the class name "show-fixed" and "hide-fixed" to make the content always appears. -->
-                <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-                <p class="pad-lft">&#0169; 2015 Your Company</p>
-            </footer>
-            <!--===================================================-->
-            <!-- END FOOTER -->
-            <!-- SCROLL TOP BUTTON -->
-            <!--===================================================-->
-            <button id="scroll-top" class="btn"><i class="fa fa-chevron-up"></i></button>
-            <!--===================================================-->
-        </div>
-        <!--===================================================-->
-        <!-- END OF CONTAINER -->
-        <!--JAVASCRIPT-->
-        <!--=================================================-->
-        <!--jQuery [ REQUIRED ]-->
-        <script src="<?php echo base_url('admin_assets/js/jquery-2.1.1.min.js');?>"></script>
-        <!--BootstrapJS [ RECOMMENDED ]-->
-        <script src="<?php echo base_url('admin_assets/js/bootstrap.min.js');?>"></script>
-        <!--Fast Click [ OPTIONAL ]-->
-        <script src="<?php echo base_url('admin_assets/plugins/fast-click/fastclick.min.js');?>"></script>
-        <!--Jquery Nano Scroller js [ REQUIRED ]-->
-        <script src="<?php echo base_url('admin_assets/plugins/nanoscrollerjs/jquery.nanoscroller.min.js');?>"></script>
-        <!--Metismenu js [ REQUIRED ]-->
-        <script src="<?php echo base_url('admin_assets/plugins/metismenu/metismenu.min.js');?>"></script> 
-       <!--Jasmine Admin [ RECOMMENDED ]-->
-        <script src="<?php echo base_url('admin_assets/js/scripts.js');?>"></script>
-        <!--Switchery [ OPTIONAL ]-->
-        <script src="<?php echo base_url('admin_assets/plugins/switchery/switchery.min.js');?>"></script>
-        <!--Bootstrap Select [ OPTIONAL ]-->
-        <script src="<?php echo base_url('admin_assets/plugins/bootstrap-select/bootstrap-select.min.js');?>"></script>
-        <!--Fullscreen jQuery [ OPTIONAL ]-->
-        <script src="<?php echo base_url('admin_assets/plugins/screenfull/screenfull.js');?>"></script>
-        <!--Demo script [ DEMONSTRATION ]-->
-        <script src="<?php echo base_url('admin_assets/js/demo/form-layout.js');?>"></script>
-    </body>
-</html>
+            
