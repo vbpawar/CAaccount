01dc55d2c86bb6e07d0adfb651e647add1b64932
Lalit <Lalit@Lalit-PC>
2020-10-24 19:10:54 +0530

    Report Changes

--- application/controllers/services/Report.php
+++ application/controllers/services/Report.php
@@ -7,11 +7,7 @@ class Report extends CI_Controller {
         $this->load->view('admin/navigation/menubar');
         $this->load->view('admin/report/new');
         $this->load->view('admin/js/comman_js');
-<<<<<<< HEAD
-        // $this->load->view('admin/js/reportjs');
-=======
         $this->load->view('admin/js/report/new_js');
->>>>>>> 1bfd057874f960dca33a68173d89691a82dac593
         $this->load->view('admin/footer');
     }
     public function update() {
