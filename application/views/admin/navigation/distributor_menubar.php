b551a079f380bf1f832a01c4db5540ffabbee76f
Lalit <Lalit@Lalit-PC>
2020-11-01 15:50:23 +0530

    admin,distributor,retailor menubar design

---
+++ application/views/admin/navigation/distributor_menubar.php
@@ -0,0 +1,155 @@
+
+<nav class="navbar navbar-default megamenu">
+    <div class="navbar-header">
+        <button type="button" data-toggle="collapse" data-target="#defaultmenu" class="navbar-toggle">
+            <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+        </button>
+    </div>
+    <!-- end navbar-header -->
+    <div id="defaultmenu" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+            <!-- standard drop down -->
+            <li class="dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle"> Wallet <b class="caret"></b></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#"> Wallet</a></li>
+                    <li><a href="#"> Wallet Transactions </a></li>
+                </ul>
+                <!-- end dropdown-menu -->
+            </li>
+            <!-- end standard drop down -->
+            <!-- standard drop down -->
+            <li class="dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle"> Users <b class="caret"></b></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#"> Users</a></li>
+                    
+                </ul>
+                <!-- end dropdown-menu -->
+            </li>
+            <!-- end standard drop down -->
+            
+            <!-- standard drop down -->
+            <li class="dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle"> Distributors & Retailors <b class="caret"></b></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#"> Distributors </a></li>
+                    <li><a href="#"> Retailors </a></li>
+                </ul>
+                <!-- end dropdown-menu -->
+            </li>
+            <!-- end standard drop down -->
+            <!-- standard drop down -->
+            <li class="dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle"> Bill Payment Services <b class="caret"></b></a>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#"> A </a></li>
+                    <li><a href="#"> B </a></li>
+                    <li><a href="#"> C </a></li>
+                </ul>
+                <!-- end dropdown-menu -->
+            </li>
+            <!-- end standard drop down -->
+            
+            <li>
+                <a href="#"> Transactions </a>
+            </li>
+            
+            
+            <!--All Services-->
+             <!-- standard drop down -->
+            <li class="dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle"> All Services <b class="caret"></b></a>
+                <ul class="dropdown-menu" role="menu">
+                    <!--CA certificate-->
+                    <li class="dropdown-submenu">
+                        <a href="#">CA Certificate</a>
+                        <ul class="dropdown-menu">
+                                    <li><a href="http://localhost/CAaccount/services/certificate/create">Create</a></li>
+                                    <li><a href="http://localhost/CAaccount/services/certificate/show">Show</a></li>
+                        </ul>
+                        <!-- end dropdown-menu -->
+                    </li>
+                    
+                    <!--Report-->
+                    <li class="dropdown-submenu">
+                        <a href="#">Report</a>
+                        <ul class="dropdown-menu">
+                                    <li><a href="http://localhost/CAaccount/services/report/create">Create</a></li>
+                                    <li><a href="http://[::1]/CAaccount/services/report/show">Show</a></li>
+                        </ul>
+                        <!-- end dropdown-menu -->
+                    </li>
+                    
+                    <!--Import Export-->
+                    <li class="dropdown-submenu">
+                        <a href="#">Import Export</a>
+                        <ul class="dropdown-menu">
+                                    <li><a href="http://localhost/CAaccount/services/ImportExport/create">Create</a></li>
+                                    <li><a href="http://[::1]/CAaccount/services/report/show">Show</a></li>
+                        </ul>
+                        <!-- end dropdown-menu -->
+                    </li>
+                    
+                    <!--pancard-->
+                    <li class="dropdown-submenu">
+                        <a href="#">Pancard</a>
+                        <ul class="dropdown-menu">
+                                    <li><a href="http://localhost/CAaccount/services/pancard/create">Create</a></li>
+                                    <li><a href="http://[::1]/CAaccount/services/report/show">Show</a></li>
+                        </ul>
+                        <!-- end dropdown-menu -->
+                    </li>
+                    
+                    <!--Balancesheet-->
+                    <li class="dropdown-submenu">
+                        <a href="#">Balancesheet</a>
+                        <ul class="dropdown-menu">
+                                    <li><a href="http://localhost/CAaccount/services/Balancesheet/create">Create</a></li>
+                                    <li><a href="http://[::1]/CAaccount/services/Balancesheet/show">Show</a></li>
+                        </ul>
+                        <!-- end dropdown-menu -->
+                    </li>
+                    
+                    <!--distributor-->
+                    <li class="dropdown-submenu">
+                        <a href="#">Distributor Retailor</a>
+                        <ul class="dropdown-menu">
+                                    <li><a href="http://localhost/CAaccount/services/RetailorDistributor/create">Create</a></li>
+                                    <li><a href="http://[::1]/CAaccount/services/RetailorDistributor/show">Show</a></li>
+                        </ul>
+                        <!-- end dropdown-menu -->
+                    </li>
+                    <!--income-->
+                    <li class="dropdown-submenu">
+                        <a href="#">Income</a>
+                        <ul class="dropdown-menu">
+                                    <li><a href="http://localhost/CAaccount/services/income/create">Create</a></li>
+                                    <li><a href="http://[::1]/CAaccount/services/income/show">Show</a></li>
+                        </ul>
+                        <!-- end dropdown-menu -->
+                    </li>
+                    <!--E-way Bill-->
+                    <li class="dropdown-submenu">
+                        <a href="#">E-way Bill</a>
+                        <ul class="dropdown-menu">
+                                    <li><a href="http://localhost/CAaccount/services/EwayBill/create">Create</a></li>
+                                    <li><a href="http://[::1]/CAaccount/services/EwayBill/show">Show</a></li>
+                        </ul>
+                        <!-- end dropdown-menu -->
+                    </li>
+                </ul>
+                <!-- end dropdown-menu -->
+            </li>
+            <!-- end standard drop down -->
+        </ul>
+        <!-- end nav navbar-nav -->
+    </div>
+    <!-- end #navbar-collapse-1 -->
+</nav>
+<!--include code-->
+<!-- end navbar navbar-default megamenu -->
+</div>
+</header>
+<!--===================================================-->
+<!--END NAVBAR-->
