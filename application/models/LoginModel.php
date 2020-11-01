ca123527b1c64588820607532920fb50982df6a1
vbpawar <vikaspawar3110@gmail.com>
2020-11-01 16:00:11 +0530

    added changes

--- application/models/LoginModel.php
+++ application/models/LoginModel.php
@@ -3,8 +3,20 @@
 class LoginModel extends CI_Model {
 
     public function checkauth($user,$pass) {
-       $data = $this->db->get_where("projectedbalancesheet", ['projectedid' => $this->input->get("projectedid")])->row_array();
-       
-        return $data;
+        $result = array();
+        $this->db->where('emailid', $user);  
+        $this->db->where('upassword', $pass);  
+        $query = $this->db->get('user_master');  
+        if($query->num_rows() > 0)  
+        {  
+            $result['data'] = $query->result();
+            $result['status']= true;
+        }  
+        else  
+        {  
+            $result['data'] = [];
+            $result['status']= false;     
+        }  
+        return $result;
     }
 }
