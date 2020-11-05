331a89c4fb96398612b568487fdec73ef8656186
vbpawar <vikaspawar3110@gmail.com>
2020-11-05 23:40:37 +0530

    added get apis

---
+++ application/models/GetModel.php
@@ -0,0 +1,21 @@
+<?php
+
+class GetModel extends CI_Model {
+
+    public function getdistributors() {
+        $sql = "SELECT um.firstname,um.lastname,um.userid FROM user_master um WHERE um.roleid = 2";
+            $query = $this->db->query($sql);
+    return $query->result();
+    }
+    public function getretailors() {
+        $sql = "SELECT um.firstname,um.lastname,um.userid FROM user_master um WHERE um.roleid = 3";
+            $query = $this->db->query($sql);
+    return $query->result();
+    }
+    public function getemployees() {
+        $sql = "SELECT um.firstname,um.lastname,um.userid FROM user_master um WHERE um.roleid = 4";
+            $query = $this->db->query($sql);
+    return $query->result();
+    }
+
+}
