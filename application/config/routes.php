122b3c87d45777ce6275a531bb06d4be7a20cec3
vbpawar <vikaspawar3110@gmail.com>
2020-11-13 23:38:59 +0530

    added db

--- application/config/routes.php
+++ application/config/routes.php
@@ -100,3 +100,5 @@ $route['load_pf'] = 'pf_controller/get_all_pf';
 $route['createdigital'] = 'digital_controller/add_digital_form';
 $route['updatedigital'] = 'digital_controller/update_digital_form';
 $route['load_digital'] = 'digital_controller/get_all_digital';
+
+$route['createudyog'] = 'udyog_controller/add_udyog_form';
