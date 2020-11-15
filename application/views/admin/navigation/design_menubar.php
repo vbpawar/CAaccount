8761663eba94d716bc210f435383001584435a8e
Lalit <Lalit@Lalit-PC>
2020-11-15 14:44:14 +0530

    design change

--- application/views/admin/navigation/design_menubar.php
+++ application/views/admin/navigation/design_menubar.php
@@ -1,7 +1,7 @@
 <li class="dropdown">
     <a href="#" data-toggle="dropdown" class="dropdown-toggle"> Service Designs <b class="caret"></b></a>
     <ul class="dropdown-menu" role="menu">
-        <li><a href="<?php echo base_url();?>pf_withdrawal/create"> PF Withdrawl</a></li>
+        <li><a href="<?php echo base_url();?>pf_withdrawal/show"> PF Withdrawl</a></li>
         <li><a href="<?php echo base_url();?>services/UdyogAadhar/create"> Udyog Aadhar</a></li>
         <li><a href="<?php echo base_url();?>services/ShopAct/create"> ShopAct</a></li>
 
@@ -10,10 +10,7 @@
         <li><a href="<?php echo base_url();?>services/DigitalSignService/create"> Digital Sign</a></li>
         <li><a href="<?php echo base_url();?>services/AccessControlService/create"> Access Control</a></li>
         <li><a href="<?php echo base_url();?>services/CompanyRegService/create"> Company Registration</a></li>
-<<<<<<< HEAD
 
-=======
->>>>>>> 3dd422a9cfc3d5612efd7a82a8df5721ee9555ff
         <li><a href="<?php echo base_url();?>services/GstRegistration/create">GST Registration</a></li>
 
 
