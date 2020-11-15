b95b3a5e8664c6a98d78614db029da1eb40582f2
vbpawar <vikaspawar3110@gmail.com>
2020-11-15 19:09:54 +0530

    added changes

--- application/controllers/services/RetailorDistributor.php
+++ application/controllers/services/RetailorDistributor.php
@@ -23,12 +23,10 @@ class RetailorDistributor extends CI_Controller {
     public function show() {
         $this->load->view('admin/header');
         $this->load->view('admin/css/comman_css');
-        $this->load->view('admin/css/dataModalCss');
         $this->load->view('admin/navigation/notificationbar');
         $this->load->view('admin/navigation/menubar');
         $this->load->view('admin/retailor_distributor/show');
         $this->load->view('admin/js/comman_js');
-        $this->load->view('admin/js/dataModalJavaScript');
         $this->load->view('admin/js/retailor_distributor/show_js');
         $this->load->view('admin/footer');
     }
