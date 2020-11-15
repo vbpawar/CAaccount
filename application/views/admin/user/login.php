73f2ed86cf2e812cc0a8dfbbd847022104c08df0
httperror451 <httperror451@gmail.com>
2020-11-15 22:23:16 +0530

    Added login title

--- application/views/admin/user/login.php
+++ application/views/admin/user/login.php
@@ -10,7 +10,7 @@ if(isset($session_data['Data'])){
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Login Page | SmartAdmin - Responsive admin template.</title>
+        <title> TKINFOTECH | Login </title>
         <link rel="shortcut icon" href="img/favicon.ico">
         <!--STYLESHEET-->
         <!--=================================================-->
