7142b5ed77762922fabbc29e92d4759bfb51fabe
vbpawar <vikaspawar3110@gmail.com>
2020-11-29 12:12:23 +0530

    add income changes

--- application/controllers/IncomeTax.php
+++ application/controllers/IncomeTax.php
@@ -6,154 +6,315 @@ class IncomeTax extends CI_Controller{
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('IncomeModel','service');
+        $this->load->model('ResidentialModel', 'rmodel');
+        $this->load->model('PersonalModel', 'pmodel');
+        $this->load->model('DocsModel', 'docs');
+        $this->load->model('ShopModel', 'smodel');
+        $this->load->model('IncomeModel', 'imodel');
+        $this->load->model('PartnerModel', 'parmodel');
+        $this->load->model('WalletModel', 'service');
+        date_default_timezone_set('Asia/Kolkata');
     }
     private $response = null;
     private $records = null;
 
-    //get all import export details
-    public function getincomeforms() {
-        $records = $this->service->getincomeforms();
-        if ($records != null) {
+    public function get_all_incomes()
+    {
+        $roleid = $this->input->get('roleid');
+        $userid = $this->input->get('userid');
+        $result = $this->imodel->get_details($roleid,$userid);
+        if ($result['status']) {
             $response = array(
-                'Message' => 'All data load successfully',
-                "Data" => $records,
+                'Message' => 'Income Details loaded successfully',
+                'Data' => $result['data'],
                 'Responsecode' => 200
             );
         } else {
             $response = array(
-                'Message' => 'Data Not Found',
-                'Responsecode' => 401
+                'Message' => 'Try again',
+                'Responsecode' => 402
             );
         }
-         echo json_encode($response);
-    }
-
-    //create API for documnet master
-    public function addincomeform()
-    {
-        $data = array(
-            "userid"=>$this->input->post("userid"),
-            "profession"=>$this->input->post("profession"),
-            "bussiname"=>$this->input->post("bussiname"),
-            "premisename"=>$this->input->post("premisename"),
-            "assesmentyear"=>$this->input->post("assesmentyear"),
-            "fname"=>$this->input->post("fname"),
-            "mname"=>$this->input->post("mname"),
-            "lname"=>$this->input->post("lname"),
-            "gender"=>$this->input->post("gender"),
-            "dob"=>$this->input->post("dob"),
-            "pannumber"=>$this->input->post("pannumber"),
-            "fathername"=>$this->input->post("fathername"),
-            "contact"=>$this->input->post("contact"),
-            "emailid"=>$this->input->post("emailid"),
-            "country"=>$this->input->post("country"),
-            "ustate"=>$this->input->post("ustate"),
-            "city"=>$this->input->post("city"),
-            "flat"=>$this->input->post("flat"),
-            "road"=>$this->input->post("road"),
-            "pincode"=>$this->input->post("pincode")
-        );
-    if(empty($data)){
-        $response = array(
-            'Message' => 'Missing parameter',
-            'Responsecode' => 303
-        );
-    }else{ 
-        $result = $this->service->addincomeform($data);
-        if($result['status']){ 
-            $response = array(
-                'Message' => 'Income form submited successfully',
-                'Data'=>$result['data'],
-                'Responsecode' => 200
-            );
-       }else{ 
-        $response = array(
-            'Message' => 'Try again',
-            'Responsecode' => 204
-        );
-       }
-    }
-     echo json_encode($response);
-}
-
-    //API - delete a document 
-    public function removebill()
-    {
-        $id  = $this->input->post('id');
-        if(!$id || empty($id)){
-            $response = array(
-                'Message' => 'Parameter missing',
-                'Responsecode' => 404
-            );
-        }else{
-        if($this->service->removebill($id))
-        {
-            $response = array(
-                'Message' => 'Income form removed successfully',
-                'Responsecode' => 200
-            );
-        } 
-        else
-        {
-            $response = array(
-                'Message' => 'Failed to remove',
-                'Responsecode' => 302
-            );
-        }
-    }
         echo json_encode($response);
     }
+    //create API for PF master
+    public function add_income_form()
+    {
+        //personal details
+        $pdetails = array(
+            'pan_name ' => $this->input->post('pan_name'),
+            'pan_number' => $this->input->post('pan_number'),
+            'aadhar_name' => $this->input->post('aadhar_name'),
+            'aadhar_number' => $this->input->post('aadhar_number'),
+            'contact_number' => $this->input->post('contact_number'),
+            'emailid' => $this->input->post('emailid')
+        );
+        
+        //Residential details
+        $rdetails = array(
+            'premise_name ' => $this->input->post('premise_name'),
+            'flat_number' => $this->input->post('flat_number'),
+            'road' => $this->input->post('road'),
+            'area' => $this->input->post('area'),
+            'village' => $this->input->post('village'),
+            'taluka' => $this->input->post('taluka'),
+            'district' => $this->input->post('district'),
+            'state' => $this->input->post('state'),
+            'pincode' => $this->input->post('pincode')
+        );
 
-   //API - update a service
-   public function updateincomeform(){
-    $data = array(
-        "userid"=>$this->input->post("userid"),
-        "profession"=>$this->input->post("profession"),
-        "bussiname"=>$this->input->post("bussiname"),
-        "premisename"=>$this->input->post("premisename"),
-        "assesmentyear"=>$this->input->post("assesmentyear"),
-        "fname"=>$this->input->post("fname"),
-        "mname"=>$this->input->post("mname"),
-        "lname"=>$this->input->post("lname"),
-        "gender"=>$this->input->post("gender"),
-        "dob"=>$this->input->post("dob"),
-        "pannumber"=>$this->input->post("pannumber"),
-        "fathername"=>$this->input->post("fathername"),
-        "contact"=>$this->input->post("contact"),
-        "emailid"=>$this->input->post("emailid"),
-        "country"=>$this->input->post("country"),
-        "ustate"=>$this->input->post("ustate"),
-        "city"=>$this->input->post("city"),
-        "flat"=>$this->input->post("flat"),
-        "road"=>$this->input->post("road"),
-        "pincode"=>$this->input->post("pincode")
-    );
-        $id = $this->input->post('id');
+         //Residential details of shop
+         $income_details = array(
+            'bname' => $this->input->post('bname'),
+            'natureofbuss' => $this->input->post('natureofbuss'),
+            'place_of_buss' => $this->input->post('place_of_buss'),
+            'area' => $this->input->post('s_area'),
+            'village' => $this->input->post('s_village'),
+            'taluka' => $this->input->post('s_taluka'),
+            'district' => $this->input->post('s_district'),
+            'state' => $this->input->post('s_state'),
+            'pincode' => $this->input->post('s_pincode')
+        );
+        
+        if (!empty($pdetails)) {
+            $pid = $this->pmodel->add_details($pdetails);
+        }
+        if (!empty($rdetails)) {
+            $rid = $this->rmodel->add_details($rdetails);
+        }
+        
+        $userid = $this->input->post('userid');
+        
+        if ($pid['status'] && $rid['status']) {
+            $pid = $pid['pid'];
+            $rid = $rid['rid'];
+            
+            $data = array(
+                'userid' => $userid,
+                'natureofbuss' => $this->input->post('nature_of_buss'),
+                'busstype' => $this->input->post('busstype'),
+                'pid' => $pid,
+                'rid' => $rid,
+                'sid' =>$sid
+            );
+            $partner_data = $this->input->post('partnerdata');
+           echo gettype($partner_data);
+            print_r($partner_data[0]);exit;
+//            print_r($partner_data);exit;
+            $result = $this->imodel->add_details($data);
+            if ($result['status']) {
+                $id       = $result['shopactid'];
+                //stored partner details
+    if(!empty($partner_data)){
+        foreach ($partner_data as $contact)
+        {
+            $partners = array(
+                'partner_name' => $contact['p_partner_name'],
+                'aadhar_number' => $contact['p_aadhar_number'],
+                'pan_number' => $contact['p_pan_number'],
+                'contact_number'=>$contact['p_contact_number'],
+                'emailid'=>$contact['p_emailid']
+                );
+                $partnerid = $this->parmodel->add_details($partners);
+            if($partnerid['status']){
+                $shop_partners=array( 
+                    'partnerid' => $partnerid['partnerid'],
+                    'shopactid'=>$id
+                );
+                $this->imodel->add_partner_details($shop_partners);
+                }
+        }
+    }
+       
+                $document = 'Document not uploaded';
+                if (!empty($_FILES['adhar']['name']) && !empty($_FILES['pan']['name']) && !empty($_FILES['passport']['name']) && !empty($_FILES['signature']['name']) && !empty($_FILES['shop_photo']['name'])
+                && !empty($_FILES['declaration']['name']) && !empty($_FILES['otherdoc']['name'])) {
+                  
+                    $first = array(
+                        'name'=>'Aadhar',
+                        'userid'=>$userid,
+                        'id'=>$id,
+                        'filename'=>$_FILES['adhar']['name'],
+                        'file'=>$_FILES['adhar']['tmp_name'],
+                        'table'=>'shopact_docs',
+                        'folder'=>'shopact',
+                        'prim'=>'shid'
+                    );
+                    $second = array(
+                        'name'=>'PAN',
+                        'userid'=>$userid,
+                        'id'=>$id,
+                        'filename'=>$_FILES['pan']['name'],
+                        'file'=>$_FILES['pan']['tmp_name'],
+                        'table'=>'shopact_docs',
+                        'folder'=>'shopact',
+                        'prim'=>'shid'
+                    );
+                    $third = array(
+                        'name'=>'Passport Photo',
+                        'userid'=>$userid,
+                        'id'=>$id,
+                        'filename'=>$_FILES['passport']['name'],
+                        'file'=>$_FILES['passport']['tmp_name'],
+                        'table'=>'shopact_docs',
+                        'folder'=>'shopact',
+                        'prim'=>'shid'
+                    );
+                    $four = array(
+                        'name'=>'Signature',
+                        'userid'=>$userid,
+                        'id'=>$id,
+                        'filename'=>$_FILES['signature']['name'],
+                        'file'=>$_FILES['signature']['tmp_name'],
+                        'table'=>'shopact_docs',
+                        'folder'=>'shopact',
+                        'prim'=>'shid'
+                    );
+                    $five = array(
+                        'name'=>'Shop Photo',
+                        'userid'=>$userid,
+                        'id'=>$id,
+                        'filename'=>$_FILES['shop_photo']['name'],
+                        'file'=>$_FILES['shop_photo']['tmp_name'],
+                        'table'=>'shopact_docs',
+                        'folder'=>'shopact',
+                        'prim'=>'shid'
+                    );
+                    $six = array(
+                        'name'=>'Declaration Form',
+                        'userid'=>$userid,
+                        'id'=>$id,
+                        'filename'=>$_FILES['declaration']['name'],
+                        'file'=>$_FILES['declaration']['tmp_name'],
+                        'table'=>'shopact_docs',
+                        'folder'=>'shopact',
+                        'prim'=>'shid'
+                    );
+                    $seven = array(
+                        'name'=>'Other Documents',
+                        'userid'=>$userid,
+                        'id'=>$id,
+                        'filename'=>$_FILES['otherdoc']['name'],
+                        'file'=>$_FILES['otherdoc']['tmp_name'],
+                        'table'=>'shopact_docs',
+                        'folder'=>'shopact',
+                        'prim'=>'shid'
+                    );
+                    
+                    
+                    if ($this->docs->uploaddocs($first)) {
+                        $document = 'Documents uplaoded';
+                    }
+                    if ($this->docs->uploaddocs($second)) {
+                        $document = 'Documents uplaoded';
+                    }
+                    if ($this->docs->uploaddocs($third)) {
+                        $document = 'Documents uplaoded';
+                    }
+                    if ($this->docs->uploaddocs($four)) {
+                        $document = 'Documents uplaoded';
+                    }
+                    if ($this->docs->uploaddocs($five)) {
+                        $document = 'Documents uplaoded';
+                    }
+                    if ($this->docs->uploaddocs($six)) {
+                        $document = 'Documents uplaoded';
+                    }
+                    if ($this->docs->uploaddocs($seven)) {
+                        $document = 'Documents uplaoded';
+                    }
+                   
     
-   
-        if(!$id && !$data || empty($id)){
+                }
+                $response = array(
+                    'Message' => 'Shop Act Form added successfully',
+                    'Data' => $result['data'],
+                    'Document' => $document,
+                    'Responsecode' => 200
+                );
+            } else {
+                $response = array(
+                    'Message' => 'Try again',
+                    'Responsecode' => 402
+                );
+            }
+        } else {
             $response = array(
-                'Message' => 'Parameter missing',
-                'Responsecode' => 404
-            );
-        }else{
-           $result = $this->service->updateincomeform($id,$data);
-           if($result['result']!=0 && $result['data']!=null){
-            $response = array(
-                'Message' => 'Income form change successfully updated',
-                'Data'=>$result['data'],
-                'Responsecode' => 200
-            );
-           }else{
-            $response = array(
-                'Message' => 'Sorry try again',
+                'Message' => 'Check Parameters',
                 'Responsecode' => 302
             );
-           
-           }
-       }
-   echo json_encode($response);
-   }
+            
+        }
+        echo json_encode($response);
+    }
     
+
+    public function getshopactdocs()
+    {
+        $id   = $this->input->post('shid');
+        $result = $this->docs->get_shop_docs($id);
+        echo json_encode($result);
+    }
+    public function update_status()
+    {
+        $shid    = $this->input->post('shid');
+        $data   = array(
+            'status' => $this->input->post('status'),
+            'remark' => $this->input->post('remark')
+        );
+        if($data['status'] == '3'){
+            $wallet_data = array(
+                'userid'=>$this->input->post('digital_uid'),
+                'transaction_type'=>'Credit',
+                'amount'=>$this->input->post('digital_amount'),
+                'message'=>'Credited amount of Shop Act service which is rejected by admin',
+                'transactiondate'=>date('Y-m-d h:i:s')
+               );
+                $result = $this->service->deduct_amount($wallet_data);
+        }
+        $result = $this->imodel->update_shop_status($shid, $data);
+        if ($result) {
+            $document = 'Documents not uplaoded';
+            if (!empty($_FILES['result1']['name']) && !empty($_FILES['result2']['name'])) {
+                if ($this->uploadremarks('SHOPACT', $shid, $_FILES['result1']['name'], $_FILES['result1']['tmp_name'])) {
+                    $document = 'Documents uplaoded';
+                }
+                if ($this->uploadremarks('SHOPACT', $shid, $_FILES['result2']['name'], $_FILES['result2']['tmp_name'])) {
+                    $document = 'Documents uplaoded';
+                }
+            }
+            $response = array(
+                'Message' => 'Status updated successfully',
+                'Responsecode' => 200
+            );
+        } else {
+            $response = array(
+                'Message' => 'Try Again',
+                'Responsecode' => 204
+            );
+        }
+        echo json_encode($response);
+    }
+    public function uploadremarks($service, $rowid, $filename, $file)
+    {
+        $ext    = pathinfo($filename, PATHINFO_EXTENSION);
+        $data   = array(
+            'service' => $service,
+            'rowid' => $rowid,
+            'extension' => $ext
+        );
+        $result = $this->docs->add_remark_docs($data);
+        if ($result['status']) {
+            $imgid      = $result['remarkid'];
+            $sourcePath = $file; // Storing source path of the file in a variable
+            $targetPath = "./documents/remarks/" . $imgid . "." . $ext; // Target path where file is to be stored
+            if (move_uploaded_file($sourcePath, $targetPath)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
--- application/models/IncomeModel.php
+++ application/models/IncomeModel.php
@@ -2,28 +2,53 @@
 
 class IncomeModel extends CI_Model {
 
-    public function getincomeforms() {
-        if(!empty($this->input->get("id"))){
-            $data = $this->db->get_where("incometax", ['id' => $this->input->get("id")])->row_array();
+    public function get_details($roleid,$userid) {
+        if($roleid ==1 || $roleid ==4){
+        $sql = "SELECT * FROM incometax pf 
+        JOIN personal_details pd ON pd.pid = pf.pid 
+        JOIN residential_details rd ON rd.rid = pf.rid  ORDER BY pf.inid DESC";
         }else{
-            $data = $this->db->get("incometax")->result();
+            $sql = "SELECT * FROM incometax pf 
+            JOIN personal_details pd ON pd.pid = pf.pid 
+            JOIN residential_details rd ON rd.rid = pf.rid 
+            WHERE pf.userid=$userid ORDER BY pf.inid DESC";  
         }
-        return $data;
+        $query = $this->db->query($sql);
+       $result['status'] = true;
+       $result['data'] =  $query->result();
+    return $result;
     }
-    public function addincomeform($data)
+   
+    public function add_details($data)
     {    
+        $result = [];
         if($this->db->insert('incometax', $data)){
             $id = $this->db->insert_id();
-            $result['status'] = true;
-           $result['data'] = $this->db->get_where("incometax", ['id' => $id])->row_array();
+            $sql = "SELECT * FROM incometax pf 
+            JOIN personal_details pd ON pd.pid = pf.pid 
+            JOIN residential_details rd ON rd.rid = pf.rid 
+            WHERE pf.inid = $id";
+            $query = $this->db->query($sql);
+           $result['inid'] =  $id;
+           $result['status'] = true;
+           $result['data'] =  $query->result();
          }else{
-            $result['data']=[];
             $result['status'] = false;
          }
          return $result;
     }
+    public function add_partner_details($data)
+    {
+        $this->db->insert('incometax_partners', $data); 
+
+        if ($this->db->affected_rows() > 0)
+        {
+            return TRUE;
+        }
+        return FALSE;
+    }
     public function removebill($id){
-        $this->db->where('id', $id);
+        $this->db->where('did', $id);
         if($this->db->delete('incometax')){
            return true;
          }else{
@@ -31,17 +56,17 @@ class IncomeModel extends CI_Model {
          }
     }
 
-    public function updateincomeform($id,$data)
-    {
-        if($id==0){
-            $result['result']= $this->db->insert('incometax',$data);
-            $result['data'] = [];
-        }else{
-            $this->db->where('id',$id);
-            $result['result'] = $this->db->update('incometax',$data);
-            $result['data']  = $this->db->get_where("incometax", ['id' => $id])->row_array();
+    
+        public function updatedigitalstatus($did,$data)
+        {
+            $result = false;
+            $this->db->where('inid',$did);
+            if($this->db->update('incometax',$data)){
+                $result['status'] = true;
+            }else{
+                $result['status'] = false;
+            }
+            return $result;
         }       
-        return $result; 
-    }
 
 }
