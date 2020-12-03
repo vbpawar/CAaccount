93ca4997d76e1b2948cf27f56951fde0c50d67fc
vbpawar <vikaspawar3110@gmail.com>
2020-12-03 21:19:56 +0530

    add a db connection

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://app.tkinfotech.com/';
+// $config['base_url'] = 'http://test.tkinfotech.com/';
 
 /*
 |--------------------------------------------------------------------------
