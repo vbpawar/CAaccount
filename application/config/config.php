8ba954cf2c769607e8d668ad86842ecfba782c26
vikas pawar <vikaspawar3110@gmail.com>
2020-12-19 15:05:13 +0530

    Site live
    from sangmner

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://app.tkinfotech.com/';
+$config['base_url'] = 'http://app.tkinfotech.com/';
 // $config['base_url'] = 'http://test.tkinfotech.com/';
- $config['base_url'] = 'local';
+//  $config['base_url'] = 'local';
 /*
 |--------------------------------------------------------------------------
 | Index File
