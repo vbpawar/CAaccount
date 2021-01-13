62fac71d7f78b0b41bcec43f8f69594d867ad572
vbpawar <vikaspawar3110@gmail.com>
2021-01-14 00:09:32 +0530

    add chnages for live

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://app.tkinfotech.com/';
-$config['base_url'] = 'http://test.tkinfotech.com/';
+$config['base_url'] = 'http://app.tkinfotech.com/';
+// $config['base_url'] = 'http://test.tkinfotech.com/';
   // $config['base_url'] = '';
 /*
 |--------------------------------------------------------------------------
