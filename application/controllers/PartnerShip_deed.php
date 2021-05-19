f4618923bbf97f83ceeecd4c3630a9f969d63837
vbpawar <vikaspawar3110@gmail.com>
2021-05-19 23:18:12 +0530

    change in partnership deed

---
+++ application/controllers/PartnerShip_deed.php
@@ -0,0 +1,229 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+class PartnerShip_deed extends CI_Controller
+{
+   
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('PartnerShip_model', 'pmodel');
+        $this->load->model('DocsModel', 'docs');
+        $this->load->model('WalletModel', 'service');
+        date_default_timezone_set('Asia/Kolkata');
+    }
+    private $response = null;
+    private $records = null;
+    
+
+    public function get_all_partners_data(){
+        $roleid = $this->input->get('roleid');
+        $userid = $this->input->get('userid');
+        $result = $this->pmodel->get_details($roleid,$userid);
+        $records = [];
+        if ($result['status']) {
+            for($i=0;$i<count($result['data']);$i++){
+                $temp = array('partners'=>[]);
+                $temp_1 = array('partners_investment'=>[]);
+               $p_details= $this->pmodel->get_partners_details($result['data'][$i]['deedid']); 
+               if($p_details['status']){
+                $temp = array('partners_data'=>$p_details['data']);
+               }
+               $p_details_1= $this->pmodel->get_partners_investment($result['data'][$i]['deedid']); 
+               if($p_details_1['status']){
+                $temp = array('partners_Investment_data'=>$p_details_1['data']);
+               }
+               $records[] = array_merge($result['data'][$i],$temp,$temp_1); 
+            }
+            $response = array(
+                'Message' => 'Partnership Deed Details loaded successfully',
+                'Data' => $records,
+                'Responsecode' => 200
+            );
+        } else {
+            $response = array(
+                'Message' => 'Try again',
+                'Responsecode' => 402
+            );
+        }
+        echo json_encode($response);
+    }
+    //create API for PF master
+    public function add_partnership_form()
+    {
+            $sdetails = array(
+                'userid' => $this->input->post('userid'),
+                'msg'=>$this->input->post('msg'),
+                'natureofbuss'=>$this->input->post('natureofbuss'),
+                'start_up_date'=>$this->input->post('start_up_date'),
+                'shop_address'=>$this->input->post('shop_address'),
+                'shop_name'=>$this->input->post('shop_name')
+            );
+          
+            $partner_data = $this->input->post('partnerdata');
+            $partner_data= json_decode($partner_data);
+
+            $partner_inve_data = $this->input->post('partnerinvestdata');
+            $partner_inve_data= json_decode($partner_inve_data);
+            $testdata = array(
+                'partnerdata'=>$partner_data,
+                'partner_invest'=> $partner_inve_data,
+                'shop_details'=>$sdetails
+            );
+            $result = $this->pmodel->create_partner_form($testdata);
+            if ($result['status']) {
+                $id       = $result['deedid'];
+                if (!empty($_FILES['rentaggrement']['name'])) {
+                    $second = array(
+                        'name'=>'Rent agrement',
+                        'userid'=>$sdetails['userid'],
+                        'id'=>$id,
+                        'filename'=>$_FILES['rentaggrement']['name'],
+                        'file'=>$_FILES['rentaggrement']['tmp_name'],
+                        'table'=>'deed_docs',
+                        'folder'=>'deed',
+                        'prim'=>'deedid'
+                    );
+                    if ($this->docs->uploaddocs($second)) {
+                        $document = 'Documents uplaoded';
+                    }
+                }
+                
+                if (!empty($_FILES['shopbill']['name'])) {
+                    $second = array(
+                        'name'=>'Shop Bill',
+                        'userid'=>$sdetails['userid'],
+                        'id'=>$id,
+                        'filename'=>$_FILES['shopbill']['name'],
+                        'file'=>$_FILES['shopbill']['tmp_name'],
+                        'table'=>'deed_docs',
+                        'folder'=>'deed',
+                        'prim'=>'deedid'
+                    );
+                    if ($this->docs->uploaddocs($second)) {
+                        $document = 'Documents uplaoded';
+                    }
+                }
+                if (!empty($_FILES['other_1']['name'])) {
+                    $second = array(
+                        'name'=>'Other Doc 1',
+                        'userid'=>$sdetails['userid'],
+                        'id'=>$id,
+                        'filename'=>$_FILES['other_1']['name'],
+                        'file'=>$_FILES['other_1']['tmp_name'],
+                        'table'=>'deed_docs',
+                        'folder'=>'deed',
+                        'prim'=>'deedid'
+                    );
+                    if ($this->docs->uploaddocs($second)) {
+                        $document = 'Documents uplaoded';
+                    }
+                }
+                
+                if (!empty($_FILES['other_2']['name'])) {
+                    $second = array(
+                        'name'=>'Other Doc 2',
+                        'userid'=>$sdetails['userid'],
+                        'id'=>$id,
+                        'filename'=>$_FILES['other_2']['name'],
+                        'file'=>$_FILES['other_2']['tmp_name'],
+                        'table'=>'deed_docs',
+                        'folder'=>'deed',
+                        'prim'=>'deedid'
+                    );
+                    if ($this->docs->uploaddocs($second)) {
+                        $document = 'Documents uplaoded';
+                    }
+                }
+        
+                $response = array(
+                    'Message' => 'Partnership Details added successfully',
+                    'Data' => $result,
+                    'Responsecode' => 200
+                );
+            } else {
+                $response = array(
+                    'Message' => 'Try again',
+                    'Responsecode' => 402
+                );
+            }
+       
+        echo json_encode($response);
+    }
+
+    public function get_partnership_docs()
+    {
+        $id   = $this->input->post('uid');
+        $result = $this->pmodel->get_partnership_docs($id);
+        echo json_encode($result);
+    }
+    public function update_status()
+    {
+        $id    = $this->input->post('id');
+        $data   = array(
+            'status' => $this->input->post('status'),
+            'remark' => $this->input->post('remark')
+        );
+        if($data['status'] == '3'){
+            $wallet_data = array(
+                'userid'=>$this->input->post('digital_uid'),
+                'transaction_type'=>'Credit',
+                'amount'=>$this->input->post('digital_amount'),
+                'message'=>'Credited amount of Partnership deed service which is rejected by admin',
+                'transactiondate'=>date('Y-m-d h:i:s')
+               );
+                $result = $this->service->deduct_amount($wallet_data);
+        }
+        $result = $this->pmodel->update_status($id, $data);
+        if ($result) {
+            $document = 'Documents not uplaoded';
+            if (!empty($_FILES['result1']['name']) && !empty($_FILES['result2']['name'])) {
+                if ($this->uploadremarks('DEED', $id, $_FILES['result1']['name'], $_FILES['result1']['tmp_name'])) {
+                    $document = 'Documents uplaoded';
+                }
+                if ($this->uploadremarks('DEED', $id, $_FILES['result2']['name'], $_FILES['result2']['tmp_name'])) {
+                    $document = 'Documents uplaoded';
+                }
+            }
+            $response = array(
+                'Message' => 'Status updated successfully',
+                'Responsecode' => 200
+            );
+        } else {
+            $response = array(
+                'Message' => 'Try Again',
+                'Responsecode' => 204
+            );
+        }
+        echo json_encode($response);
+    }
+    public function uploadremarks($service, $rowid, $filename, $file)
+    {
+        $ext    = pathinfo($filename, PATHINFO_EXTENSION);
+        $data   = array(
+            'service' => $service,
+            'rowid' => $rowid,
+            'extension' => $ext
+        );
+        $result = $this->docs->add_remark_docs($data);
+        if ($result['status']) {
+            $imgid      = $result['remarkid'];
+            $sourcePath = $file; // Storing source path of the file in a variable
+            $targetPath = "./documents/remarks/" . $imgid . "." . $ext; // Target path where file is to be stored
+            if (move_uploaded_file($sourcePath, $targetPath)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+   
+    public function get_update_docs()
+    {
+        $pfid   = $this->input->post('rowid');
+        $result = $this->docs->get_update_remarks_docs($pfid,'DEED');
+        echo json_encode($result);
+    }
+} 
---
+++ application/models/PartnerShip_model.php
@@ -0,0 +1,226 @@
+<?php
+
+class PartnerShip_model extends CI_Model {
+
+    public function get_details($roleid,$userid) {
+        $result = [];
+        if($roleid ==1 || $roleid ==4){
+        $sql = "SELECT pd.deedid,pd.userid,pd.shop_name,pd.shop_address,pd.start_up_date,pd.natureofbuss,pd.msg,pd.status,pd.remark,
+        pd.createdat,pd.updateda,um.firstname,um.lastname
+        FROM partnership_deed pd INNER JOIN user_master um ON um.userid = pd.userid ORDER BY pd.deedid DESC";
+                  }else{
+                    $sql = "SELECT pd.deedid,pd.userid,pd.shop_name,pd.shop_address,pd.start_up_date,pd.natureofbuss,pd.msg,pd.status,pd.remark,
+                    pd.createdat,pd.updateda,um.firstname,um.lastname
+                    FROM partnership_deed pd INNER JOIN user_master um ON um.userid = pd.userid WHERE pd.userid =$userid ORDER BY pd.deedid DESC";  
+                }
+        $query = $this->db->query($sql);
+            $result['status'] = true;
+            $result['data'] =  $query->result_array();       
+        return $result;
+    }   
+    public function get_partners_details($id)
+    {
+        $sql = "SELECT * FROM `partnership_deed_partners` WHERE deedid= $id";
+        $query = $this->db->query($sql);
+        if($query->num_rows()>0){
+            $result['status'] = true;
+            $result['data'] =  $query->result();
+        }else{
+            $result['status'] = false;
+        }
+     return $result;
+    }
+
+    public function get_partners_investment($id)
+    {
+        $sql = "SELECT * FROM `partnership_partner_investment` WHERE deedid= $id";
+        $query = $this->db->query($sql);
+        if($query->num_rows()>0){
+            $result['status'] = true;
+            $result['data'] =  $query->result();
+        }else{
+            $result['status'] = false;
+        }
+     return $result;
+    }
+          
+        public function updatestatus($id,$data)
+        {
+            $result = false;
+            $this->db->where('gid',$id);
+            if($this->db->update('gst_service',$data)){
+                $result['status'] = true;
+            }else{
+                $result['status'] = false;
+            }
+            return $result;
+        }
+
+        public function get_partnership_docs($pfid) {
+            $sql = "SELECT * FROM deed_docs WHERE deedid=$pfid";
+            $query = $this->db->query($sql);
+            return $query->result_array();
+        }
+        
+ public function create_partner_form($data)
+    {
+      $partner_invest = $data['partner_invest'];
+      $shop_details = $data['shop_details'];
+      $partnerdata = $data['partnerdata'];
+      $result = array();
+      $this->db->trans_begin();
+
+        $this->db->insert('partnership_deed', $shop_details);
+        $result['deedid'] =  $this->db->insert_id(); 
+
+        $this->getpartnerdetails($partnerdata,$result['deedid']);
+        $this->getinvestmentdetails($partner_invest,$result['deedid'])
+        if ($this->db->trans_status() === FALSE)
+            {
+                $this->db->trans_rollback();
+            }
+        else
+        {
+        $this->db->trans_commit();
+      
+        $result['status'] = true;
+        return $result;
+        }
+    }
+    public function getpartnerdetails($partner_data,$id)
+    {
+        foreach ($partner_data as $contact)
+                    {
+                      
+                            $edoc = '';
+                            $adoc = '';
+                            $pandoc = '';
+                          
+                            if(isset($contact->electricity)){
+                                $edoc= $this->upload_file($contact->electricity);
+                            }
+                            if(isset($contact->aadhar)){
+                                $adoc= $this->upload_file($contact->aadhar);
+                                }
+                               
+                                    if(isset($contact->pan)){
+                                      $pandoc=  $this->upload_file($contact->pan);
+                                        }
+                                        $partners = array(
+                                            'deedid'=>$id,
+                                            'partner_name' => $contact->partner_name,
+                                            'aadhar_number' => $contact->aadhar_number,
+                                            'pan_name'=>$contact->pan_name,
+                                            'pan_number' => $contact->pan_number,
+                                            'mobile_number'=>$contact->mobile_number,
+                                            'emailid'=>$contact->emailid,
+                                            'electricity'=> $edoc,
+                                            'aadhar'=> $adoc,
+                                            'pan'=> $pandoc
+                                            );
+                                            $this->db->insert('partnership_deed_partners', $partners);
+                                            $partnerid =  $this->db->insert_id();
+                    }
+    }  
+    function upload_file($encoded_string){
+        $target_dir = './documents/deed/partners/'; // add the specific path to save the file
+        $filedata = explode(',', $encoded_string);
+         $decoded_file = base64_decode($filedata[1]);
+        //$decoded_file = base64_decode($encoded_string); // decode the file
+        $mime_type = finfo_buffer(finfo_open(), $decoded_file, FILEINFO_MIME_TYPE); // extract mime type
+        $extension = $this->mime2ext($mime_type); // extract extension from mime type
+        $file = uniqid() .'.'. $extension; // rename file as a unique name
+        $file_dir = $target_dir . uniqid() .'.'. $extension;
+        try {
+            file_put_contents($file_dir, $decoded_file); // save
+            return $file_dir;
+            // database_saving($file);
+            // header('Content-Type: application/json');
+            // echo json_encode("File Uploaded Successfully");
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            // header('Content-Type: application/json');
+            // echo json_encode($e->getMessage());
+        }
+    
+    }
+    function mime2ext($mime){
+        $all_mimes = '{"png":["image\/png","image\/x-png"],"bmp":["image\/bmp","image\/x-bmp",
+        "image\/x-bitmap","image\/x-xbitmap","image\/x-win-bitmap","image\/x-windows-bmp",
+        "image\/ms-bmp","image\/x-ms-bmp","application\/bmp","application\/x-bmp",
+        "application\/x-win-bitmap"],"gif":["image\/gif"],"jpeg":["image\/jpeg",
+        "image\/pjpeg"],"xspf":["application\/xspf+xml"],"vlc":["application\/videolan"],
+        "wmv":["video\/x-ms-wmv","video\/x-ms-asf"],"au":["audio\/x-au"],
+        "ac3":["audio\/ac3"],"flac":["audio\/x-flac"],"ogg":["audio\/ogg",
+        "video\/ogg","application\/ogg"],"kmz":["application\/vnd.google-earth.kmz"],
+        "kml":["application\/vnd.google-earth.kml+xml"],"rtx":["text\/richtext"],
+        "rtf":["text\/rtf"],"jar":["application\/java-archive","application\/x-java-application",
+        "application\/x-jar"],"zip":["application\/x-zip","application\/zip",
+        "application\/x-zip-compressed","application\/s-compressed","multipart\/x-zip"],
+        "7zip":["application\/x-compressed"],"xml":["application\/xml","text\/xml"],
+        "svg":["image\/svg+xml"],"3g2":["video\/3gpp2"],"3gp":["video\/3gp","video\/3gpp"],
+        "mp4":["video\/mp4"],"m4a":["audio\/x-m4a"],"f4v":["video\/x-f4v"],"flv":["video\/x-flv"],
+        "webm":["video\/webm"],"aac":["audio\/x-acc"],"m4u":["application\/vnd.mpegurl"],
+        "pdf":["application\/pdf","application\/octet-stream"],
+        "pptx":["application\/vnd.openxmlformats-officedocument.presentationml.presentation"],
+        "ppt":["application\/powerpoint","application\/vnd.ms-powerpoint","application\/vnd.ms-office",
+        "application\/msword"],"docx":["application\/vnd.openxmlformats-officedocument.wordprocessingml.document"],
+        "xlsx":["application\/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application\/vnd.ms-excel"],
+        "xl":["application\/excel"],"xls":["application\/msexcel","application\/x-msexcel","application\/x-ms-excel",
+        "application\/x-excel","application\/x-dos_ms_excel","application\/xls","application\/x-xls"],
+        "xsl":["text\/xsl"],"mpeg":["video\/mpeg"],"mov":["video\/quicktime"],"avi":["video\/x-msvideo",
+        "video\/msvideo","video\/avi","application\/x-troff-msvideo"],"movie":["video\/x-sgi-movie"],
+        "log":["text\/x-log"],"txt":["text\/plain"],"css":["text\/css"],"html":["text\/html"],
+        "wav":["audio\/x-wav","audio\/wave","audio\/wav"],"xhtml":["application\/xhtml+xml"],
+        "tar":["application\/x-tar"],"tgz":["application\/x-gzip-compressed"],"psd":["application\/x-photoshop",
+        "image\/vnd.adobe.photoshop"],"exe":["application\/x-msdownload"],"js":["application\/x-javascript"],
+        "mp3":["audio\/mpeg","audio\/mpg","audio\/mpeg3","audio\/mp3"],"rar":["application\/x-rar","application\/rar",
+        "application\/x-rar-compressed"],"gzip":["application\/x-gzip"],"hqx":["application\/mac-binhex40",
+        "application\/mac-binhex","application\/x-binhex40","application\/x-mac-binhex40"],
+        "cpt":["application\/mac-compactpro"],"bin":["application\/macbinary","application\/mac-binary",
+        "application\/x-binary","application\/x-macbinary"],"oda":["application\/oda"],
+        "ai":["application\/postscript"],"smil":["application\/smil"],"mif":["application\/vnd.mif"],
+        "wbxml":["application\/wbxml"],"wmlc":["application\/wmlc"],"dcr":["application\/x-director"],
+        "dvi":["application\/x-dvi"],"gtar":["application\/x-gtar"],"php":["application\/x-httpd-php",
+        "application\/php","application\/x-php","text\/php","text\/x-php","application\/x-httpd-php-source"],
+        "swf":["application\/x-shockwave-flash"],"sit":["application\/x-stuffit"],"z":["application\/x-compress"],
+        "mid":["audio\/midi"],"aif":["audio\/x-aiff","audio\/aiff"],"ram":["audio\/x-pn-realaudio"],
+        "rpm":["audio\/x-pn-realaudio-plugin"],"ra":["audio\/x-realaudio"],"rv":["video\/vnd.rn-realvideo"],
+        "jp2":["image\/jp2","video\/mj2","image\/jpx","image\/jpm"],"tiff":["image\/tiff"],
+        "eml":["message\/rfc822"],"pem":["application\/x-x509-user-cert","application\/x-pem-file"],
+        "p10":["application\/x-pkcs10","application\/pkcs10"],"p12":["application\/x-pkcs12"],
+        "p7a":["application\/x-pkcs7-signature"],"p7c":["application\/pkcs7-mime","application\/x-pkcs7-mime"],"p7r":["application\/x-pkcs7-certreqresp"],"p7s":["application\/pkcs7-signature"],"crt":["application\/x-x509-ca-cert","application\/pkix-cert"],"crl":["application\/pkix-crl","application\/pkcs-crl"],"pgp":["application\/pgp"],"gpg":["application\/gpg-keys"],"rsa":["application\/x-pkcs7"],"ics":["text\/calendar"],"zsh":["text\/x-scriptzsh"],"cdr":["application\/cdr","application\/coreldraw","application\/x-cdr","application\/x-coreldraw","image\/cdr","image\/x-cdr","zz-application\/zz-winassoc-cdr"],"wma":["audio\/x-ms-wma"],"vcf":["text\/x-vcard"],"srt":["text\/srt"],"vtt":["text\/vtt"],"ico":["image\/x-icon","image\/x-ico","image\/vnd.microsoft.icon"],"csv":["text\/x-comma-separated-values","text\/comma-separated-values","application\/vnd.msexcel"],"json":["application\/json","text\/json"]}';
+        $all_mimes = json_decode($all_mimes,true);
+        foreach ($all_mimes as $key => $value) {
+            if(array_search($mime,$value) !== false) return $key;
+        }
+        return false;
+    }
+    public function update_status($id,$data)
+    {
+        $result = false;
+        $this->db->where('deedid',$id);
+        if($this->db->update('partnership_deed',$data)){
+            $result['status'] = true;
+        }else{
+            $result['status'] = false;
+        }
+        return $result;
+    } 
+    public function getinvestmentdetails($partner_data,$id)
+    {
+        foreach ($partner_data as $contact)
+                    {
+                      
+                        
+                                        $partners = array(
+                                            'deedid' =>$id,
+                                            'partner_name' => $contact->partner_name,
+                                            'investment'=>$contact->investment
+                                            );
+                                            $this->db->insert('partnership_partner_investment', $partners);
+                                            $partnerid =  $this->db->insert_id();
+                    }
+    }  
+}
+?>
